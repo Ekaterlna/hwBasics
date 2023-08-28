812e86e73bf3c1e65a52d7b77bfc2a06dab6eef8
Ekaterina Korotkova <ekaterinakorotkova300799@gmail.com>
2023-08-28 11:12:21 +0300

    Change framework and add tests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,31 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import org.testng.Assert;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldRemain0IfAmount1000() {
+        assertEquals(service.remain(1000), 0);
+    }
+
+    @Test
+    public void shouldRemain1000IfAmount0 () {
+        assertEquals(service.remain(0), 1000);
+    }
+
+    @Test
+    public void shouldRemain1IfAmount999() {
+        assertEquals(service.remain(999), 1);
+    }
+
+    @Test
+    public void shouldRemain999IfAmount1() {
+        assertEquals(service.remain(1), 999);
+    }
+}
