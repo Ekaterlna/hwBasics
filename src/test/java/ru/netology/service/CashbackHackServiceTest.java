848c541b91da2ec5c932bb970fd063355e4cf73d
Ekaterina Korotkova <ekaterinakorotkova300799@gmail.com>
2023-08-28 12:23:40 +0300

    Change framework and change test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,10 +1,7 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
-
-import org.testng.Assert;
-
-import static org.testng.Assert.assertEquals;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
