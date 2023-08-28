f558be0d146fc9a95cca6f882b60d8e343ee035c
Ekaterina Korotkova <ekaterinakorotkova300799@gmail.com>
2023-08-28 13:17:55 +0300

    change framework and add javaTestClass

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTestJupiter.java
@@ -0,0 +1,28 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CashbackHackServiceTestJupiter {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldRemain0IfAmount1000() {
+        assertEquals(0, service.remain(1000));
+    }
+
+    @Test
+    public void shouldRemain1000IfAmount0 () {
+        assertEquals(1000, service.remain(0));
+    }
+
+    @Test
+    public void shouldRemain1IfAmount999() {
+        assertEquals(1, service.remain(999));
+    }
+
+    @Test
+    public void shouldRemain999IfAmount1() {
+        assertEquals(999, service.remain(1));
+    }
+}
